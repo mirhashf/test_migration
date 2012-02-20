2c7e005829bcc393c8f5f94cc13a0ae654ca3e05
AJ Minich <aj.minich@binatechnologies.com>
2012-02-20 22:05:32 +0000

    Fixing sym link creation.

--- ajminich/scripts/aws/configure.sh
+++ ajminich/scripts/aws/configure.sh
@@ -6,9 +6,9 @@
 DATA_FOLDER="/mnt/data"
 mkdir -p ${DATA_FOLDER}
 chmod a+rw ${DATA_FOLDER}
-ln -s /mnt/data /home/ubuntu/data
+ln -s ${DATA_FOLDER} /home/ubuntu/
 
 EXEC_FOLDER="/mnt/execution"
 mkdir ${EXEC_FOLDER}
 chmod a+rw ${EXEC_FOLDER}
-ln -s /mnt/execution /home/ubuntu/execution
+ln -s ${EXEC_FOLDER}  /home/ubuntu/
