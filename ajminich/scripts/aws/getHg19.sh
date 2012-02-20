5d21b2e12ee1250ea1c044e65757c9411fd37411
AJ Minich <aj.minich@binatechnologies.com>
2012-02-20 12:10:36 -0800

    Whoops, switched my scripts up.

--- ajminich/scripts/aws/getHg19.sh
+++ ajminich/scripts/aws/getHg19.sh
@@ -6,6 +6,6 @@ DATA_FOLDER="/mnt/data/hg19/"
 
 echo "Getting hg19 chr21 reference data."
 mkdir -p ${DATA_FOLDER}
-s3cmd get s3://bina.data/hg19/chr21.* ${DATA_FOLDER}
+s3cmd get s3://bina.data/hg19/ucsc.hg19.* ${DATA_FOLDER}
 
 echo "Completed downloading chr21 reference data."
