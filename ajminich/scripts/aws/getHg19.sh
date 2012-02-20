3e74ae9a110360379d1d5a653586ddb232cdc69b
AJ Minich <aj.minich@binatechnologies.com>
2012-02-20 12:03:50 -0800

    Adding script to download all of hg19 reference data.

---
+++ ajminich/scripts/aws/getHg19.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Gets the reference data for all of hg19.
+
+DATA_FOLDER="/mnt/data/hg19/"
+
+echo "Getting hg19 chr21 reference data."
+mkdir -p ${DATA_FOLDER}
+s3cmd get s3://bina.data/hg19/chr21.* ${DATA_FOLDER}
+
+echo "Completed downloading chr21 reference data."
