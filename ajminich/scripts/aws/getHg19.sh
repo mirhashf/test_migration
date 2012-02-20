865f751bfd3e94167f18203bc0338c545b91263c
AJ Minich <aj.minich@binatechnologies.com>
2012-02-20 22:04:53 +0000

    Adding skip-existing option to downloaders so they avoid already-completed downloads.

--- ajminich/scripts/aws/getHg19.sh
+++ ajminich/scripts/aws/getHg19.sh
@@ -6,6 +6,6 @@ DATA_FOLDER="/mnt/data/hg19/"
 
 echo "Getting hg19 reference data."
 mkdir -p ${DATA_FOLDER}
-s3cmd get s3://bina.data/hg19/ucsc.hg19.* ${DATA_FOLDER}
+s3cmd get --skip-existing s3://bina.data/hg19/ucsc.hg19.* ${DATA_FOLDER}
 
 echo "Completed downloading hg19 reference data."
