ba361456e04661a84104f615870436438bdc4eec
AJ Minich <aj.minich@binatechnologies.com>
2012-04-24 12:26:03 -0700

    Updating configure script for running SeqAlto on EC2.

--- ajminich/scripts/aws/configure.sh
+++ ajminich/scripts/aws/configure.sh
@@ -1,17 +1,36 @@
 #! /bin/bash
 
-# configure.sh
-# Configures the scratchdisk.
+# Setup script for running SeqAlto on EC2
+# Note: run this script with sudo so it can create and write directories.
 
-DATA_FOLDER="/mnt/data"
-mkdir -p ${DATA_FOLDER}
-chmod a+rw ${DATA_FOLDER}
-ln -s ${DATA_FOLDER} /home/ubuntu/
+# Get the most recent SeqAlto binary and set up the symbolic link
+echo "Setting up most recent version of SeqAlto aligner from S3."
+s3cmd get s3://bina.programs/aligner.aws ~/programs/aligner.aws
+chmod a+x ~/programs/aligner.aws
+rm ~/bin/seqalto
+ln -s ~/bin/seqalto ~/programs/aligner.aws
 
-EXEC_FOLDER="/mnt/execution"
-mkdir ${EXEC_FOLDER}
-chmod a+rw ${EXEC_FOLDER}
-ln -s ${EXEC_FOLDER}  /home/ubuntu/
+# Set up /ebs
+echo "Setting up EBS persistent storage."
+chmod a+rw /ebs
+mkdir /ebs/data
+mkdir /ebs/execution
 
-# Give permission to read, write, and edit /mnt
+# Set up /mnt
 chmod a+rw /mnt
+
+# Set up needed files
+echo "Getting FASTA Index and generating Seqalto index."
+s3cmd get s3://bina.data/hg19/ucsc.hg19.fasta /ebs/data/hg19.fa
+seqalto -mode index \
+    -ref /ebs/data/hg19.fa \
+    -index_mode 0 \
+    -kmer_size 22 \
+    -index_name /ebs/data/hg19.fa_22.sidx \
+    -logtostderr
+
+
+
+
+
+
