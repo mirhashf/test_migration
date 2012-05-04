f898a6920ad5a507ecdb4c72d3e1920714805859
AJ Minich <aj.minich@binatechnologies.com>
2012-05-04 13:56:52 -0700

    Updating script for new Snyder run.

--- ajminich/aws/seqaltoAlign.ec2.sh
+++ ajminich/aws/seqaltoAlign.ec2.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
 # SeqAlto Alignment for Snyder Reads Data
 
 if [[ ${#} -lt 2 ]]; then
   echo "SeqAlto Alignment Script"
   echo "Written by Jian Li, April 2012"
+  echo "Modified by AJ Minich, May 2012"
   echo ""
   echo "Use: sudo ${0} <read_prefix>_#.fq.gz <library>"
   echo "  <read_prefix> - the read from s3://seqalto/ to use (for example, A804NLABXX.s_5)"
@@ -11,55 +13,85 @@ if [[ ${#} -lt 2 ]]; then
   return
 fi
 
+LANE=${1}
+LIB=${2}
+
 TMP=/mnt
+QUAL=30
+REGION=chr1
+DATA_FOLDER=/snyder/data/
+EXECUTION_FOLDER=/snyder/execution/hg19.major/
+
+SEQALTO_OUT_SAM=${EXECUTION_FOLDER}/seqalto_${LANE}.sam
+SEQALTO_OUT_BAM=${EXECUTION_FOLDER}/seqalto_${LANE}.bam
+SORTED_BAM=${EXECUTION_FOLDER}/seqalto_${LANE}.sorted.bam
+SORTED_REGION_BAM=${EXECUTION_FOLDER}/seqalto_${LANE}.${REGION}.csorted.bam
+SORTED_AG_REGION_BAM=${EXECUTION_FOLDER}/seqalto_${LANE}.${REGION}_AG.bam
+
+echo "Running Seqalto alignment on Snyder lane ${LANE} from sample {$LIB}."
 
 # Get the reads and trim them to quality level 30
-sudo s3cmd get s3://seqalto/$1_*.fq.gz ~/data/
-sudo gunzip ~/data/$1_*.fq.gz
-sudo ~/bin/trim ~/data/$1 30 
+s3cmd get s3://seqalto/${LANE}_*.fq.gz ${DATA_FOLDER}
+
+echo "Unzipping lanes..."
+gunzip ${DATA_FOLDER}/${LANE}_*.fq.gz
+
+echo "Trimming lanes..."
+~/sandbox/algorithms/alignment/trim.sh ${DATA_FOLDER}/$1 ${QUAL}
+
+# Remove the original FASTQ files
+rm ${DATA_FOLDER}/${LANE}_1.fq
+rm ${DATA_FOLDER}/${LANE}_2.fq
 
 # Run the alignment
 sudo time ~/bin/seqalto -mode align \
-	-1 ~/data/$1_trimmed_1.fq \
-	-2 ~/data/$1_trimmed_2.fq \
-	-idx ~/data/hg19.fa_22.sidx \
+	-1 ${DATA_FOLDER}/${LANE}_trimmed_1.fq \
+	-2 ${DATA_FOLDER}/${LANE}_trimmed_2.fq \
+	-idx /ebs/data/hg19.major.fa_22.sidx \
 	--template_len_comp_method 2 \
 	--enable_batch_pairing \
 	-p 8 \
 	-h -1 -nw_disable_match_at_ends \
 	-verbose \
 	2>&1 \
-	1>/ebs/execution/seqalto_$1.sam | tee seqalto_$1.log
+	1>${SEQALTO_OUT_SAM} | tee ${EXECUTION_FOLDER}/seqalto_${LANE}.log
 
 # Convert to BAM and sort by coordinate
-sudo /bin/samtools view -bS /ebs/execution/seqalto_$1.sam > /ebs/execution/seqalto_$1.bam
+echo "Converting to BAM..."
+sudo /bin/samtools view -bS ${SEQALTO_OUT_SAM} > ${SEQALTO_OUT_BAM}
+rm ${SEQALTO_OUT}
+
 sudo java -Xms5g -Xmx5g -jar ~/programs/picard/dist/SortSam.jar \
-	INPUT=/ebs/execution/seqalto_$1.bam \
+	INPUT=${SEQALTO_OUT_BAM} \
 	VALIDATION_STRINGENCY=LENIENT \
-	OUTPUT=/ebs/execution/seqalto_$1.csorted.bam \
+	OUTPUT=${SORTED_BAM} \
 	SORT_ORDER=coordinate \
 	TMP_DIR=$TMP
 
+# Remove now-unnecessary raw BAM file
+rm ${SEQALTO_OUT_BAM}
+
 sudo java -Xms5g -Xmx5g -jar ~/programs/picard/dist/BuildBamIndex.jar \
-	INPUT=/ebs/execution/seqalto_$1.csorted.bam \
+	INPUT=${SORTED_BAM} \
 	VALIDATION_STRINGENCY=LENIENT
 
-# Get just the reads aligned to chr1
-sudo /bin/samtools view -h /ebs/execution/seqalto_$1.csorted.bam chr1 > /ebs/execution/seqalto_$1.chr1.sam
-sudo /bin/samtools view -bS /ebs/execution/seqalto_$1.chr1.sam > /ebs/execution/seqalto_$1.chr1.bam
+# Get just the reads aligned to the selected region
+echo "Getting reads in the region: ${REGION}"
+sudo /bin/samtools view -bh ${SORTED_BAM} \
+    ${REGION} > ${SORTED_REGION_BAM}
 
 # Perform add/replace read groups
 java -Xms5g -Xmx5g -jar ~/programs/picard/dist/AddOrReplaceReadGroups.jar \
-	I=/ebs/execution/seqalto_$1.chr1.bam \
-	O=/ebs/execution/seqalto_$1.chr1_AG.bam  \
+	I=${SORTED_REGION_BAM} \
+	O=${SORTED_AG_REGION_BAM} \
 	SORT_ORDER=coordinate \
-	RGID=$1 \
-	RGLB=$2 \
+	RGID=${LANE} \
+	RGLB=${LIB} \
 	RGPL=ILLUMINA \
-	RGSM=$2 \
+	RGSM=${LIB} \
 	RGPU=HiSeq \
 	VALIDATION_STRINGENCY=LENIENT  \
-	TMP_DIR=$TMP
+	TMP_DIR=${TMP}
 
 echo "Execution complete."
 
