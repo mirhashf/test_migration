02af631498c69520a35406b63681dfd8a52ae002
AJ Minich <aj.minich@binatechnologies.com>
2012-05-04 15:05:25 -0700

    Improving script for running Snyder on blood 5-8 and saliva A808 1-8.

--- ajminich/aws/runSnyder.sh
+++ ajminich/aws/runSnyder.sh
@@ -1,19 +1,41 @@
 #!/bin/bash
 
-# Runs Snyder on the remaining lanes.
+# Runs the Snyder EC2 script on the specified lanes, and uploads the results to EC2.
 
-for i in {1..5}
-do
+SNYDER_EC2_SCRIPT=~/sandbox/ajminich/aws/seqaltoAlign.ec2.sh
+S3_FOLDER=s3://bina.results/snyder/hg19.major/
 
-  ~/sandbox/jianl/seqaltoAlign.ec2.sh A808HKABXX.s_${i} SLB
+bloodPrefix="A804NLABXX.s_"
+bloodStart=6
+bloodEnd=8
 
-  s3cmd put ~/execution/seqalto_A808HKABXX.s_${i}.chr1_AG.bam s3://bina.results/snyder/
-  s3cmd put ~/execution/seqalto_A808HKABXX.s_${i}.log s3://bina.results.snyder/
+salivaPrefix="A808HKABXX.s_"
+salivaStart=1
+salivaEnd=8
 
-  # Cleanup
-  rm -f ~/execution/*.sam
-  rm -f ~/execution/*${i}.bam
-  rm -f ~/data/*.fq
+function processAll() {
+    
+    prefix=${1}
+    startIndex=${2}
+    stopIndex=${3}
+    
+    for (( index=${startIndex}; index<=${stopIndex}; index++ ))
+    do
+    
+        filename=${prefix}${index}
+        echo "Processing Snyder lane ${filename}."
 
-done
+        ${SNYDER_EC2_SCRIPT} ${filename} 2>&1 | tee seqalto_${filename}.log
 
+        # Upload the final BAM file and runlog to S3
+        s3cmd put seqalto_${filename}.chr1.csorted.bam ${S3_FOLDER}
+        s3cmd put seqalto_${filename}.log ${S3_FOLDER}
+    
+        echo "Snyder lane ${filename} complete."
+        
+    done
+
+}
+
+processAll ${bloodPrefix} ${bloodStart} ${bloodEnd}
+processAll ${salivaPrefix} ${salivaStart} ${salivaEnd}
