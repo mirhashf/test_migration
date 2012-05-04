9a175662b0f255c5d7cfac0aff7cd461ea53f7c9
AJ Minich <aj.minich@binatechnologies.com>
2012-05-04 15:31:35 -0700

    Updating docs and fixing some bugs in region extractor.

--- algorithms/alignment/extractRegion.sh
+++ algorithms/alignment/extractRegion.sh
@@ -6,29 +6,41 @@ if [[ ${#} -lt 4 ]]; then
   echo "Region Extraction Script"
   echo "Written by Jian Li and AJ Minich, April-May 2012"
   echo ""
-  echo "Use: sudo ${0} <inputBAM> <sortedBAM> <region> <finalFile>"
-  echo "  <inputFile> - raw alignment file (as SAM or BAM) to "
+  echo "Use: sudo ${0} <inputBAM> <region> <finalFile>"
+  echo "  <inputFile> - raw alignment file (as SAM or BAM) to extract regions from"
+  echo "  <region>    - the region to extract"
+  echo "  <finalFile> - the final BAM file to write extracted regions to"
+  echo ""
+  echo "Notes:"
+  echo "- Input file will not be sorted if it is already in coordinate order."
+  echo "- If sorting is needed, sorted BAM file will be written to <inputFile>.sorted"
   
   exit 0
 fi
 
 INPUT_FILE=${1}
-SORTED_BAM=${2}
-REGION=${3}
-FINAL_BAM=${4}
+REGION=${2}
+FINAL_BAM=${3}
+
+TMP=/tmp
 
 header=`/bin/samtools view -h ${INPUT_FILE}`
 if [[ "${header}" == *SO:coordinate* ]]
 then
   echo "Input file is already sorted by coordinate.";
+  
+  SORTED_BAM=${INPUT_FILE}
+  
 else
+
+  SORTED_BAM=${INPUT_FILE}.sorted
 
     sudo java -Xms5g -Xmx5g -jar ~/programs/picard/dist/SortSam.jar \
         INPUT=${INPUT_FILE} \
         VALIDATION_STRINGENCY=LENIENT \
         OUTPUT=${SORTED_BAM} \
         SORT_ORDER=coordinate \
-        TMP_DIR=$TMP
+        TMP_DIR=${TMP}
 fi
 
 sudo java -Xms5g -Xmx5g -jar ~/programs/picard/dist/BuildBamIndex.jar \
