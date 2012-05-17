8385baab2fe721609d5e7c645ec052041e9d1dd1
AJ Minich <aj.minich@binatechnologies.com>
2012-05-17 01:27:09 +0000

    Adding script to mark and remove duplicates.

---
+++ jianl/markCMD.sh
@@ -0,0 +1,28 @@
+
+TMP=/mnt
+PICARD=~/programs/picard/dist
+d=$1
+f=$2
+
+sudo java -Xms5g -Xmx5g -jar $PICARD/BuildBamIndex.jar INPUT=$d/$f.bam VALIDATION_STRINGENCY=LENIENT
+
+echo ">>> Marking duplicates"
+
+sudo java -Xms5g -Xmx5g -jar $PICARD/MarkDuplicates.jar \
+        TMP_DIR=$TMP \
+        I=$d/$f.bam\
+        O=$d/$f\_marked.bam\
+        M=$d/$f.metrics \
+        VALIDATION_STRINGENCY=SILENT \
+        ASSUME_SORTED=true \
+        REMOVE_DUPLICATES=true \
+        READ_NAME_REGEX=".+:[0-9]+:([0-9]+):([0-9]+):([0-9]+).*" 
+
+#        OPTICAL_DUPLICATE_PIXEL_DISTANCE=100
+
+echo "*** Finished removing duplicates ***"
+
+sudo java -Xms5g -Xmx5g -jar $PICARD/BuildBamIndex.jar INPUT=$d/$f\_marked.bam VALIDATION_STRINGENCY=LENIENT
+
+echo "DONE"
+
