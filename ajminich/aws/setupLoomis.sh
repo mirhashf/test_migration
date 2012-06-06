1fbcdae40b86252c0c5e74c0c9dab22fb87acde0
AJ Minich <aj.minich@binatechnologies.com>
2012-06-05 17:30:16 -0700

    Adding setup script for running Loomis on EC2.

---
+++ ajminich/aws/setupLoomis.sh
@@ -0,0 +1,35 @@
+# !/bin/bash
+
+# Setup script for running Loomis on EC2.
+# Should be run as root.
+
+# For more information, please visit the Loomis guide at
+# https://binatechnologies.atlassian.net/wiki/display/SEQ/Loomis+User%27s+Guide
+
+# Directories and files
+HOME=/home/ubuntu/
+ZOOKEEPER=${HOME}/zookeeper-3.3.5/
+SEQALTO=${HOME}/seqalto/
+DATA=/mnt/data/
+EXECUTION=/mnt/execution/
+
+# Build necessary files
+cd ${SEQALTO}
+scons aligner -j24
+scons samsorter -j24
+ant dist -file loomis/server/build.xml
+
+# Get data
+mkdir ${DATA}
+chmod a+rw ${DATA}
+cd ${DATA}
+s3cmd get s3://bina.data/hg19/ucsc.hg19.fasta
+s3cmd get s3://bina.data/dbSNP/dbsnp.hg19.vcf
+s3cmd get s3://bina.data/svdata/windows.tar.gz
+tar -xf windows.tar.gz
+
+# Start Zookeeper
+${ZOOKEEPER}/bin/zkServer.sh start
+
+# Setup complete
+echo "Setup complete: all resources successfully downloaded/initialized."
