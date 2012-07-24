5d534e5d542a38a652fbdd647bc16a0997b4e7ff
AJ Minich <aj.minich@binatechnologies.com>
2012-07-23 19:22:46 -0700

    FMI script now runs all FMI data.

--- ajminich/jobs/fmi.sh
+++ ajminich/jobs/fmi.sh
@@ -3,10 +3,29 @@
 # Runs the FMI datasets through Loomis.
 
 FMI_DATA_FOLDER="/mnt/scratch0/public/data/FMI"
-FIRST_END_SUFFIX=".read1.fq"
-SECOND_END_SUFFIX=".read2.fq"
+FIRST_END_SUFFIX="read1.fq"
+SECOND_END_SUFFIX="read2.fq"
+
+BINA_DATA_PATH="bina://data/FMI"
+BINA_RESULTS_PATH="bina://jobs/FMI"
+
+HOSTS="milan-00:8080 milan-01:8080"
+KEY="gocardinal"
 
 # Get list of prefixes to process
-files=`ls ${FMI_DATA_FOLDER} | cut -f1,2 -d'.' | uniq`
+files=`ls ${FMI_DATA_FOLDER}/*.fq | cut -f1,2 -d'.' | uniq`
 
-echo ${files}
+for file in ${files};
+do
+    sample_name=`basename ${file}`
+    
+    echo "Queueing up FMI sample ${sample_name}."
+    bina --hosts ${HOSTS} --key ${KEY} run \
+        -r ${BINA_DATA_PATH}/${sample_name}.${FIRST_END_SUFFIX} \
+        -R ${BINA_DATA_PATH}/${sample_name}.${SECOND_END_SUFFIX} \
+        -rg ${sample_name} -lb FMI -sm ${sample_name} \
+        -o ${BINA_RESULTS_PATH} \
+        --desc "FMI Sample ${sample_name}"
+done
+
+echo "All jobs queued."
