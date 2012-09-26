c5216cc12378e9569da4c4cd93a03311df33622e
AJ Minich <aj.minich@binatechnologies.com>
2012-09-26 16:06:27 -0700

    Adding VQSR to Stanford example, based on Cuiping's script.

--- ajminich/jobs/stanford_example.py
+++ ajminich/jobs/stanford_example.py
@@ -119,6 +119,71 @@ job.structural_variation.set_run_pindel(True)
 job.structural_variation.pindel.set_use_breakdancer(True)
 
 '''
+        VQSR
+'''
+
+job.genotyping.set_perform_vqsr(True)
+
+# Set up variant quality training resources.
+# Assumes that the bina:// path is pointed to /srv/gs1/projects/snyder/cuiping.
+
+hapmap_resource = bina.VariantRecalibrationResource("hapmap",
+    "bina://data/referencefiles/hapmap/hapmap_3.3.hg19.majorchr.sites.vcf")
+hapmap_resource.set_prior(15.0)
+hapmap_resource.set_known(False)
+hapmap_resource.set_training(True)
+hapmap_resource.set_truth(True)
+
+omni_resource = bina.VariantRecalibrationResource("omni",
+    "bina://data/referencefiles/omni/1000G_omni2.5.hg19.majorchr.sites.vcf")
+omni_resource.set_prior(12.0)
+omni_resource.set_known(False)
+omni_resource.set_training(True)
+omni_resource.set_truth(False)
+
+dbsnp_resource = bina.VariantRecalibrationResource("dbsnp",
+    "bina://data/referencefiles/dbsnp/dbsnp_135.majorchr.hg19.vcf")
+omni_resource.set_prior(6.0)
+omni_resource.set_known(True)
+omni_resource.set_training(False)
+omni_resource.set_truth(False)
+
+mills_resource = bina.VariantRecalibrationResource("mills",
+    "bina://data/referencefiles/indel/Mills_and_1000G_gold_standard.indels.hg19.sites.vcf")
+mills_resource.set_prior(12.0)
+mills_resource.set_known(True)
+mills_resource.set_training(True)
+mills_resource.set_truth(True)
+
+# SNP Recalibration
+recal_operation = bina.VariantRecalOperation()
+recal_operation.set_name("SNP recalibration")
+recal_operation.set_variant_type("SNP")
+
+recal_operation.variant_recalibrator.set_option("--use_annotation",
+    "QD,HaplotypeScore,MQRankSum,ReadPosRankSum,HRun")
+recal_operation.apply_recalibration.set_option("--ts_filter_level", 99.0)
+
+recal_operation.add_resource(hapmap_resource)
+recal_operation.add_resource(omni_resource)
+recal_operation.add_resource(dbsnp_resource)
+
+job.genotyping.add_recal_operation(recal_operation)
+
+# Indel Recalibration based on 1000 Genomes index variants
+recal_operation = bina.VariantRecalOperation()
+recal_operation.set_name("indel recalibration")
+recal_operation.set_variant_type("INDEL")
+
+recal_operation.variant_recalibrator.set_option("--use_annotation",
+    "QD,FS,HaplotypeScore,ReadPosRankSum")
+recal_operation.apply_recalibration.set_option("--ts_filter_level", 99.0)
+
+recal_operation.add_resource(mills_resource)
+
+job.genotyping.add_recal_operation(recal_operation)
+
+'''
         JOB SUBMISSION
 '''
 
