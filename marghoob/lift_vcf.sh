e3ddfdb1926e820fe4474f8eb84070551a044ab3
Marghoob Mohiyuddin <marghoobm@binatechnologies.com>
2013-10-14 16:08:00 -0700

    More checks

--- marghoob/lift_vcf.sh
+++ marghoob/lift_vcf.sh
@@ -7,12 +7,31 @@ function usage {
   exit 1
 }
 
+# copied from http://stackoverflow.com/questions/3685970/bash-check-if-an-array-contains-a-value
+function contains() {
+  local n=$#
+  local value=${!n}
+  for ((i=1;i < $#;i++)) {
+    if [ "${!i}" == "${value}" ]; then
+      echo "y"
+      return 0
+    fi
+  }
+  echo "n"
+  return 1
+}
+
+
 [ $# -ne 4 ] && usage
 
 src=$1
 dst=$2
 inputvcf=$3
 outputvcf=$4
+
+refids=("b36" "b37" "hg18" "hg19")
+[ $(contains "${refids[@]}" $src) == "n" ] && echo "Src refname $src non-existent" && exit 1
+[ $(contains "${refids[@]}" $dst) == "n" ] && echo "Dst refname $dst non-existent" && exit 1
 
 export JAVA_HOME=$HOME/lake/opt/jdk1.7.0_25/
 export PATH=$HOME/vcftools_0.1.11/bin:$JAVA_HOME/bin:$PATH
@@ -23,5 +42,7 @@ export b37=$HOME/lake/users/marghoob/ftp.broadinstitute.org/bundle/2.5/b37/human
 export hg19=$HOME/lake/users/marghoob/GATK-bundle-hg19/ucsc.hg19
 
 CHAIN=$CHAIN_DIR/"$src"to"$dst".chain
+
+[ ! -f "$CHAIN" ] && echo "Chain file $CHAIN doesn't exist" && exit 1
 
 $GATK_DEV/public/perl/liftOverVCF.pl -vcf $inputvcf -chain $CHAIN -out $outputvcf -gatk $GATK_DEV -newRef ${!dst} -oldRef ${!src} -tmp $PWD
