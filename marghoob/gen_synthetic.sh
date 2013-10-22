e8b7ff7828b4d0b10d0b1bf97eb4df3893ba48af
Marghoob Mohiyuddin <marghoob@tehran-00.binatechnologies.com>
2013-10-22 12:44:00 -0700

    Scripts for generating synthetic genome and running variant-calling.

---
+++ marghoob/gen_synthetic.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+set -ex
+
+NIST=$HOME/lake/users/marghoob/NIST/NISThighConf
+GATKVCF=~/lake/users/marghoob/GATK-bundle-hg19/CEUTrio.HiSeq.WGS.b37.bestPractices.phased.hg19.vcf.gz
+DELETIONS_VCF=~/lake/users/marghoob/homes.gersteinlab.org/people/aabyzov/outbox/private/NA12878_variants/NA12878.2010_06.and.fosmid.deletions.phased.vcf
+
+CHR_LIST="chr1"
+SCRATCHDIR=$HOME/scratch/dwgsim
+WORKDIR=$PWD/work
+LOGDIR=$WORKDIR/log
+mkdir -p $WORKDIR
+mkdir -pv $SCRATCHDIR
+
+# Tool vars
+LIFTVCF=~/git/sandbox/marghoob/lift_vcf.sh
+GATK_JAR=/home/marghoob/lake/opt/CancerAnalysisPackage-2013.2-18-g8207e53/GenomeAnalysisTK.jar
+VCF2DIPLOID=~/lake/users/marghoob/vcf2diploid/vcf2diploid.jar
+IGVTOOLS=~/lake/users/marghoob/IGVTools/igvtools.jar
+DWGSIM=~/lake/opt/dwgsim/dwgsim
+SAMTOOLS=/usr/lib/bina/samtools/current/bin/samtools
+
+REFERENCE=~/lake/users/marghoob/GATK-bundle-hg19/ucsc.hg19.fa
+
+cd $WORKDIR
+
+# Create the bedfile for the chromosomes specified
+if [ -z "$CHR_LIST" ]; then
+  CHR_LIST=`awk '{print $1}' $REFERENCE.fai`
+fi
+
+if (( 0 )); then
+
+# Prepare the right VCF for deletions
+echo "Lifting the deletion SVs from b36 to b37 after removing records with incorrect filter and removing the END field in INFO column"
+awk '/^#/ { str = $0; sub(/-1/, "1", str); print str} !/^#/ { str = $0; sub(/END=[0-9]+;/, "", str); if ($7 != "0") print str}' $DELETIONS_VCF > deletions.b36.vcf
+$LIFTVCF b36 b37 deletions.b36.vcf deletions.b37.vcf &>$LOGDIR/deletions.b36.to.b37.log
+echo "Lifting the deletion SVs from b37 to hg19"
+$LIFTVCF b37 hg19 deletions.b37.vcf deletions.hg19.vcf &>$LOGDIR/deletions.b37.to.hg19.log
+
+# Prepare the right VCF for SNPs
+for var in SNP INDEL
+do
+  echo "Extracting $var from $GATKVCF"
+  (java -Xms1g -Xmx1g -jar $GATK_JAR -T SelectVariants -R $REFERENCE --sample_name NA12878 -selectType $var -V $GATKVCF --excludeFiltered -o CEUTrio.PASS.$var.hg19.vcf -U LENIENT_VCF_PROCESSING &>$LOGDIR/$var.PASS.log; bgzip -f CEUTrio.PASS.$var.hg19.vcf && tabix -f CEUTrio.PASS.$var.hg19.vcf.gz) &
+  (java -Xms1g -Xmx1g -jar $GATK_JAR -T SelectVariants -R $REFERENCE --sample_name NA12878 -selectType $var -V $GATKVCF -o CEUTrio.ALL.$var.hg19.vcf -U LENIENT_VCF_PROCESSING &>$LOGDIR/$var.ALL.log; bgzip -f CEUTrio.ALL.$var.hg19.vcf && tabix -f CEUTrio.ALL.$var.hg19.vcf.gz) &
+done
+wait
+
+for var in SNP INDEL
+do
+  (echo "Extracting $var missed from NIST"
+  vcf-isec -c $NIST.$var.hg19.annotated.vcf.gz CEUTrio.PASS.$var.hg19.vcf.gz | bgzip > NIST.$var.missed.in.pass.vcf.gz; tabix -f NIST.$var.missed.in.pass.vcf.gz
+
+  echo "Extracting $var present in ALL, but not in PASS and in NIST"
+  vcf-isec CEUTrio.ALL.$var.hg19.vcf.gz NIST.$var.missed.in.pass.vcf.gz | bgzip > NIST.$var.present.in.all.vcf.gz; tabix -f NIST.$var.present.in.all.vcf.gz
+
+  echo "Extracting $var not present in ALL but in NIST"
+  vcf-isec -c NIST.$var.missed.in.pass.vcf.gz NIST.$var.present.in.all.vcf.gz | bgzip > NIST.$var.missed.in.all.vcf.gz; tabix -f NIST.$var.missed.in.all.vcf.gz) &
+done
+wait
+
+for vcf in *.vcf.gz
+do
+  (gunzip $vcf && java -Xms1g -Xmx1g -jar $IGVTOOLS index $vcf) &
+done
+wait
+fi
+
+# Build the genome
+mkdir -pv vcf2diploid
+cd vcf2diploid
+#java -jar $VCF2DIPLOID -id NA12878 -chr $REFERENCE -vcf ../deletions.hg19.vcf ../CEUTrio.PASS.INDEL.hg19.vcf ../NIST.INDEL.present.in.all.vcf ../NIST.INDEL.missed.in.all.vcf ../CEUTrio.PASS.SNP.hg19.vcf ../NIST.SNP.present.in.all.vcf ../NIST.SNP.missed.in.all.vcf &>$LOGDIR/vcf2diploid.log
+
+echo "Getting list of chromosomes"
+all_list=
+
+for chr in $CHR_LIST; do
+  if [ -e "$chr"_NA12878_maternal.fa ]; then
+    all_list="$all_list $chr""_NA12878_maternal.fa"
+  fi
+  if [ -e "$chr"_NA12878_paternal.fa ]; then
+    all_list="$all_list $chr""_NA12878_paternal.fa"
+  fi
+done
+
+cat $all_list > NA12878.fa
+$SAMTOOLS faidx NA12878.fa
+
+cd ..
+
+mkdir -pv fastq
+for lane in 0; do
+  $DWGSIM -e 0.0001 -E 0.0001 -r 0 -F 0 -d 350 -C 18 -1 100 -2 100 -z $lane vcf2diploid/NA12878.fa fastq/simulated.lane$lane
+
+  (gzip -1 -f fastq/simulated.lane$lane.bwa.read1.fastq)&
+  (gzip -1 -f fastq/simulated.lane$lane.bwa.read2.fastq)&
+  wait
+done
---
+++ marghoob/run_variant_calling.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+set -ex
+
+export JAVA_HOME=~/lake/opt/jdk1.7.0_25/
+export PATH=$HOME/vcftools_0.1.11/bin:$JAVA_HOME/bin:$PATH:/usr/lib/bina/samtools/current/bin/
+CHR_LIST="chr1"
+
+GATK_JAR=$HOME/lake/opt/gatk-2.7-2-g6bda569/GenomeAnalysisTK.jar
+BWARUNNER_DIR=$PWD/bina/bwarunner/current/bin
+BWARUNNER=$BWARUNNER_DIR/bwarunner.py
+BWA=$PWD/bina/bwa/current/bin/bwa
+SAMTOOLS=$PWD/bina/samtools/current/bin/samtools
+BGZIP=$PWD/bina/tabix/current/bin/bgzip
+TABIX=$PWD/bina/tabix/current/bin/tabix
+BWAINDEX=$PWD/indexes/bwaindex
+HEADER=$PWD/header.sam
+
+myname=`basename $0`
+
+function usage {
+  echo "FASTQ1=<reads1> FASTQ2=<reads2> WORKDIR=<workdir> $myname"
+  exit 1
+}
+
+[ -z "$FASTQ1" ] && usage
+[ -z "$FASTQ2" ] && usage
+[ -z "$WORKDIR" ] && usage
+
+WORKDIR=$PWD/$WORKDIR
+LOGDIR=$WORKDIR/logs
+
+mkdir -pv $WORKDIR
+mkdir -pv $LOGDIR
+
+dbsnp=~/lake/users/marghoob/GATK-bundle-hg19/dbsnp_137.hg19.vcf
+reference=~/lake/users/marghoob/GATK-bundle-hg19/ucsc.hg19.fa
+
+pushd $PWD
+cd $WORKDIR
+
+JAVA_TMP=$WORKDIR/tmp
+
+INTERVALS=
+REGIONS=
+for chr in $CHR_LIST; do
+  awk -v chr=$chr '{if ($1 == chr) { print $1" 0 "$2 }}' $reference.fai > $chr.bed
+  INTERVALS="$INTERVALS -L $chr.bed"
+  if [ -z "$REGIONS" ]; then
+    REGIONS="-r $chr"
+  else
+    REGIONS="$REGIONS,$chr"
+  fi
+done
+
+cat $HEADER <($BWARUNNER --bwa $BWA --splitter $BWARUNNER_DIR/fastq_splitter --merger $BWARUNNER_DIR/sam_merger --reference $BWAINDEX --reads1 $FASTQ1 --reads2 $FASTQ2 --bwa_aln_options '-t 4 -q 30' --bwa_samse_options ' -r "@RG	ID:lane0	SM:NA12878	LB:pairedend"' --bwa_sampe_options '-P -r "@RG	ID:lane0	SM:NA12878	LB:pairedend"' --log_directory $LOGDIR/bwarunner_log.0 --working_directory $WORKDIR/bwarunner_work --compression_level 1 --nthreads 8 --splitter_blocksize '65536' --splitter_max_pending '4' 2>$LOGDIR/aligner.0.log) | $SAMTOOLS view -b -1 -S - > bwa.aligned.bam
+$SAMTOOLS sort -m 50000000000 bwa.aligned.bam bwa.aligned.sorted
+$SAMTOOLS index bwa.aligned.sorted.bam
+
+# Create the bed file
+
+java -Djava.io.tmpdir=$JAVA_TMP -jar $GATK_JAR -T RealignerTargetCreator --input_file bwa.aligned.sorted.bam --out realign.intervals --reference_sequence $reference $INTERVALS 2>&1 1>$LOGDIR/realigner_target_creator.log
+
+java -Djava.io.tmpdir=$JAVA_TMP -jar $GATK_JAR -T IndelRealigner --input_file bwa.aligned.sorted.bam --out realigned.bam --reference_sequence $reference --targetIntervals realign.intervals 2>&1 1>$LOGDIR/indel_realigner.log
+$SAMTOOLS index realigned.bam
+
+java -Djava.io.tmpdir=$JAVA_TMP -jar $GATK_JAR -T BaseRecalibrator -cov 'ReadGroupCovariate' -cov 'QualityScoreCovariate' -cov 'CycleCovariate' -cov 'ContextCovariate' --input_file realigned.bam $INTERVALS --reference_sequence $reference -o recal.table -knownSites $dbsnp &>$LOGDIR/base_recalibrator.log
+
+java -Djava.io.tmpdir=$JAVA_TMP -jar $GATK_JAR -T PrintReads --input_file realigned.bam --out recalibrated.bam --reference_sequence $reference -BQSR recal.table -baq RECALCULATE $INTERVALS &>$LOGDIR/print_reads.log
+$SAMTOOLS index recalibrated.bam
+
+java -Djava.io.tmpdir=$JAVA_TMP -jar $GATK_JAR -T UnifiedGenotyper --input_file recalibrated.bam --out genotyped.vcf --reference_sequence $reference --dbsnp $dbsnp $INTERVALS -stand_emit_conf 0.1 -baq CALCULATE_AS_NECESSARY --genotype_likelihoods_model BOTH -A AlleleBalance -A Coverage -A MappingQualityZero -nt 8 &>$LOGDIR/unified_genotyper.log
+
+$BGZIP -f genotyped.vcf
+$TABIX -f genotyped.vcf.gz
+
+for vartype in SNP INDEL; do
+  (java -Djava.io.tmpdir=$JAVA_TMP -jar $GATK_JAR -T SelectVariants -V genotyped.vcf.gz -o genotyped.$vartype.vcf -selectType $vartype -R $reference &>$LOGDIR/SelectVariants.$vartype.log; $BGZIP -f genotyped.$vartype.vcf && $TABIX -f genotyped.$vartype.vcf.gz) &
+done
+wait
+
+for vartype in SNP INDEL; do
+  vcf-compare $REGIONS genotyped.$vartype.vcf.gz /home/marghoob/lake/users/marghoob/NIST/NISThighConf.$vartype.hg19.vcf.gz > NIST.sensitivity.$vartype.txt
+done
+
+popd
