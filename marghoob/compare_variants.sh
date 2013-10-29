5a72f506fa2657d22d3df545bfdff282dc44266f
Marghoob Mohiyuddin <marghoob@tehran-00.binatechnologies.com>
2013-10-28 18:34:25 -0700

    Cleaned up scripts

--- marghoob/compare_variants.sh
+++ marghoob/compare_variants.sh
@@ -1,54 +1,88 @@
 #!/bin/bash
 
 function usage {
-  echo "compare_variants.sh <jobdir1> <jobdir2> <workdir> <summaryfile>"
+  echo "REFCALLS= compare_variants.sh <job1> <job2> <workdir> <summaryfile>"
   exit 1
 }
 
-set -e
 export PERL5LIB=$HOME/vcftools_0.1.11/lib/perl5/site_perl
 export JAVA_HOME=$HOME/lake/opt/jdk1.7.0_25/
 export PATH=$HOME/vcftools_0.1.11/bin:$JAVA_HOME/bin:$PATH
 export GATK_JAR=$HOME/lake/opt/gatk-2.7-2-g6bda569/GenomeAnalysisTK.jar
-export SNPSIFT=$HOME/lake/users/marghoob/snpEff/SnpSift.jar
+export SNPSIFT=$HOME/lake/opt/snpEff/SnpSift.jar
 dbsnp=$HOME/lake/users/marghoob/GATK-bundle-hg19/dbsnp_137.hg19.vcf
 reference=$HOME/lake/users/marghoob/GATK-bundle-hg19/ucsc.hg19.fa
 
-dir1=$1
-dir2=$2
-outdir=$3
-outfile=$4
+function print_abs_path {
+  echo $(cd $(dirname $1); pwd)/$(basename $1)
+}
+
+function merge_vcfs {
+  local vcfdir=$1
+  local reference=$2
+  local outfile=$3
+
+  local file_list=
+  for chr in `awk '{ print $1 }' $reference.fai`; do
+    [ -e "$vcfdir/$chr.vcf.gz" ] && file_list="$vcfdir/$chr.vcf.gz $file_list"
+  done
+
+  echo "Concatening vcfs from $vcfdir"
+  (vcf-concat $file_list | bgzip > $outfile; tabix -f $outfile) &
+}
+
+function annotate_vcf {
+  local invcf=$1
+  local outvcf=$2
+  local logfile=$3
+
+  (java  -Xmx1g -Xms1g -jar $SNPSIFT annotate $dbsnp <(gunzip -c $invcf|awk 'BEGIN{OFS="\t"} /^#/{print $0} !/^#/{printf("%s\t%s\t.",$1,$2); for(i=4;i<=NF;++i)printf("\t%s", $i);printf("\n")}') 2>$logfile | bgzip > $outvcf; tabix -f $outvcf) &
+}
+
+function filter_and_select_vcf {
+  local invcf=$1
+  local outvcf=$2
+  local vartype=$3
+  local filter=$4
+  local logfile=$5
+
+  local exclude_filter=""
+  [ "$filter" == "PASS" ] && exclude_filter="--excludeFiltered"
+
+  (java -Xmx1g -Xms1g -jar $GATK_JAR -T SelectVariants -U LENIENT_VCF_PROCESSING -selectType $vartype $exclude_filter -env -V $invcf -o $outvcf -R $reference &>$logfile; bgzip -f $outvcf; tabix -f $outvcf.gz) &
+}
+
+set -e
+
 ANNOTATE="true"
 
-[ -z "$dir1" ] && usage
-[ -z "$dir2" ] && usage
-[ -z "$outdir" ] && usage
-[ -z "$outfile" ] && usage
+[ -z "$1" ] && usage
+[ -z "$2" ] && usage
+[ -z "$3" ] && usage
+[ -z "$4" ] && usage
+[ -z "$REFCALLS" ] && REFCALLS=false
+
+dir1=$(print_abs_path $1)
+dir2=$(print_abs_path $2)
+outdir=$(print_abs_path $3)
+outfile=$(print_abs_path $4)
 
 rm -f $outfile
 
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 mkdir -pv $outdir
-awk -v dir1=$dir1 '{print dir1"/vcfs/"$1".vcf.gz"}' $reference.fai > $outdir/files1
-awk -v dir2=$dir2 '{print dir2"/vcfs/"$1".vcf.gz"}' $reference.fai > $outdir/files2
 
-echo "Concatenating the chromosome VCFs"
-vcf1=$outdir/all1.vcf.gz
-vcf2=$outdir/all2.vcf.gz
-
-(vcf-concat -f $outdir/files1 | bgzip > $outdir/all1.pre_annotated.vcf.gz; tabix -f $outdir/all1.pre_annotated.vcf.gz) &
-(vcf-concat -f $outdir/files2 | bgzip > $outdir/all2.pre_annotated.vcf.gz; tabix -f $outdir/all2.pre_annotated.vcf.gz) &
+[ -d "$dir1" ] && merge_vcfs $dir1 $reference $outdir/all1.pre_annotated.vcf.gz || (cd $outdir && ln -f -s $dir1 all1.pre_annotated.vcf.gz && ln -f -s $dir1.tbi all1.pre_annotated.vcf.gz.tbi)
+[ -d "$dir2" ] && merge_vcfs $dir2 $reference $outdir/all2.pre_annotated.vcf.gz || (cd $outdir && ln -f -s $dir2 all2.pre_annotated.vcf.gz && ln -f -s $dir2.tbi all2.pre_annotated.vcf.gz.tbi)
 wait
 
 if [ "$ANNOTATE" == "true" ]
 then
 echo "Annotating variants"
 start_time=$(date +%s)
-(java -Xmx1g -Xms1g -jar $SNPSIFT annotate $dbsnp <(gunzip -c $outdir/all1.pre_annotated.vcf.gz|awk 'BEGIN{OFS="\t"} /^#/{print $0} !/^#/{printf("%s\t%s\t.",$1,$2); for(i=4;i<=NF;++i)printf("\t%s", $i);printf("\n")}') 2>$outdir/snpsift1.log | bgzip > $outdir/all1.vcf.gz; tabix -f $outdir/all1.vcf.gz) &
-(java -Xmx1g -Xms1g -jar $SNPSIFT annotate $dbsnp <(gunzip -c $outdir/all2.pre_annotated.vcf.gz|awk 'BEGIN{OFS="\t"} /^#/{print $0} !/^#/{printf("%s\t%s\t.",$1,$2); for(i=4;i<=NF;++i)printf("\t%s", $i);printf("\n")}') 2>$outdir/snpsift2.log | bgzip > $outdir/all2.vcf.gz; tabix -f $outdir/all2.vcf.gz) &
-#(java -Xmx1g -Xms1g -jar $GATK_JAR -T VariantAnnotator -nt 8 -U LENIENT_VCF_PROCESSING -R $reference -D $dbsnp --variant $outdir/all1.pre_annotated.vcf.gz --out $outdir/all1.vcf &>$outdir/annotation1.log; bgzip -f $outdir/all1.vcf; tabix -f $outdir/all1.vcf.gz)
-#(java -Xmx1g -Xms1g -jar $GATK_JAR -T VariantAnnotator -nt 8 -U LENIENT_VCF_PROCESSING -R $reference -D $dbsnp --variant $outdir/all2.pre_annotated.vcf.gz --out $outdir/all2.vcf &>$outdir/annotation2.log; bgzip -f $outdir/all2.vcf; tabix -f $outdir/all2.vcf.gz)
+annotate_vcf $outdir/all1.pre_annotated.vcf.gz $outdir/all1.vcf.gz $outdir/snpsift1.log
+annotate_vcf $outdir/all2.pre_annotated.vcf.gz $outdir/all2.vcf.gz $outdir/snpsift2.log
 wait
 end_time=$(date +%s)
 diff_time=$(( $end_time - $start_time ))
@@ -58,57 +92,53 @@ else
 (mv $outdir/all2.pre_annotated.vcf.gz $outdir/all2.vcf.gz; tabix -f $outdir/all2.vcf.gz)
 fi
 
-echo "Separating into indels and SNPs"
-for vartype in SNP INDEL
-do
-
-(java -Xmx1g -Xms1g -jar $GATK_JAR -T SelectVariants -U LENIENT_VCF_PROCESSING -selectType $vartype -V $vcf1 -o $outdir/"$vartype"1.vcf -R $reference --excludeFiltered &>$outdir/"$vartype"1.log; bgzip -f $outdir/"$vartype"1.vcf; tabix -f $outdir/"$vartype"1.vcf.gz) &
-(java -Xmx1g -Xms1g -jar $GATK_JAR -T SelectVariants -U LENIENT_VCF_PROCESSING -selectType $vartype -V $vcf2 -o $outdir/"$vartype"2.vcf -R $reference --excludeFiltered &>$outdir/"$vartype"2.log; bgzip -f $outdir/"$vartype"2.vcf; tabix -f $outdir/"$vartype"2.vcf.gz) &
-
+for filter in ALL PASS; do
+  for vartype in SNP INDEL; do
+    echo "Separating out $vartype for $filter calls"
+    filter_and_select_vcf $outdir/all1.vcf.gz $outdir/$filter.$vartype.1.vcf $vartype $filter $outdir/$filter.$vartype.1.log
+    filter_and_select_vcf $outdir/all2.vcf.gz $outdir/$filter.$vartype.2.vcf $vartype $filter $outdir/$filter.$vartype.2.log
+  done
 done
 wait
 
 echo "Comparing the indel and SNP vcfs and getting stats"
-for vartype in SNP INDEL
-do
-vcf-compare -a $outdir/"$vartype"1.vcf.gz $outdir/"$vartype"2.vcf.gz > $outdir/$vartype.vcf-compare.txt &
-vcf-stats $outdir/$vartype"1.vcf.gz" -p $outdir/$vartype.1.stats &
-vcf-stats $outdir/$vartype"2.vcf.gz" -p $outdir/$vartype.2.stats &
+for filter in ALL PASS; do
+  for vartype in SNP INDEL; do
+    vcf-compare $outdir/$filter.$vartype.1.vcf.gz $outdir/$filter.$vartype.2.vcf.gz > $outdir/$filter.$vartype.vcf-compare.txt &
+    vcf-stats $outdir/$filter.$vartype.1.vcf.gz -p $outdir/$filter.$vartype.1.stats &
+    vcf-stats $outdir/$filter.$vartype.2.vcf.gz -p $outdir/$filter.$vartype.2.stats &
+  done
 done
+wait
 
 echo "Generating known and novel subsets of INDELS and SNPs"
-for vartype in SNP INDEL
-do
 
-mkdir -p $outdir/$vartype
+for filter in ALL PASS; do
+  for vartype in SNP INDEL; do
+    mkdir -pv $outdir/$filter/$vartype
 
-f1=$outdir/"$vartype"1.vcf.gz
-f2=$outdir/"$vartype"2.vcf.gz
+    f1=$outdir/$filter.$vartype.1.vcf.gz
+    f2=$outdir/$filter.$vartype.2.vcf.gz
 
-(vcf-isec $f1 $f2 | bgzip > $outdir/$vartype/common.vcf.gz; tabix -f $outdir/$vartype/common.vcf.gz) &
-(vcf-isec -c $f1 $f2 | bgzip >  $outdir/$vartype/1.vcf.gz; tabix -f $outdir/$vartype/1.vcf.gz) &
-(vcf-isec -c $f2 $f1 | bgzip > $outdir/$vartype/2.vcf.gz; tabix -f $outdir/$vartype/2.vcf.gz) &
-wait
+    (vcf-isec $f1 $f2 | bgzip > $outdir/$filter/$vartype/common.vcf.gz; tabix -f $outdir/$filter/$vartype/common.vcf.gz) &
+    (vcf-isec -c $f1 $f2 | bgzip > $outdir/$filter/$vartype/1.vcf.gz; tabix -f $outdir/$filter/$vartype/1.vcf.gz) &
+    (vcf-isec -c $f2 $f1 | bgzip > $outdir/$filter/$vartype/2.vcf.gz; tabix -f $outdir/$filter/$vartype/2.vcf.gz) &
+    wait
 
-for subset in common 1 2
-do
+    for subset in common 1 2; do
+      mkdir -pv $outdir/$filter/$vartype/$subset
+      (cat <(gunzip -c $outdir/$filter/$vartype/$subset.vcf.gz|grep "^#") <(gunzip -c $outdir/$filter/$vartype/$subset.vcf.gz |grep -v "^#"|awk '{if ($3 != ".") print $0}') | bgzip > $outdir/$filter/$vartype/$subset/known.vcf.gz; tabix -f $outdir/$filter/$vartype/$subset/known.vcf.gz) &
+      (cat <(gunzip -c $outdir/$filter/$vartype/$subset.vcf.gz|grep "^#") <(gunzip -c $outdir/$filter/$vartype/$subset.vcf.gz |grep -v "^#"|awk '{if ($3 == ".") print $0}') | bgzip > $outdir/$filter/$vartype/$subset/novel.vcf.gz; tabix -f $outdir/$filter/$vartype/$subset/novel.vcf.gz) &
+      wait
 
-mkdir -p $outdir/$vartype/$subset
-(cat <(gunzip -c $outdir/$vartype/$subset.vcf.gz|grep "^#") <(gunzip -c $outdir/$vartype/$subset.vcf.gz |grep -v "^#"|awk '{if ($3 != ".") print $0}') | bgzip > $outdir/$vartype/$subset/known.vcf.gz; tabix -f $outdir/$vartype/$subset/known.vcf.gz) &
-(cat <(gunzip -c $outdir/$vartype/$subset.vcf.gz|grep "^#") <(gunzip -c $outdir/$vartype/$subset.vcf.gz |grep -v "^#"|awk '{if ($3 == ".") print $0}') | bgzip > $outdir/$vartype/$subset/novel.vcf.gz; tabix -f $outdir/$vartype/$subset/novel.vcf.gz) &
-wait
+      for subsubset in known novel; do
+        vcf-stats $outdir/$filter/$vartype/$subset/$subsubset.vcf.gz -p $outdir/$filter/$vartype/$subset/$subsubset &
+      done
 
-for subsubset in known novel
-do
-vcf-stats $outdir/$vartype/$subset/$subsubset.vcf.gz -p $outdir/$vartype/$subset/$subsubset &
+      vcf-stats $outdir/$filter/$vartype/$subset.vcf.gz -p $outdir/$filter/$vartype/$subset.stats &
+    done
+  done
 done
-
-vcf-stats $outdir/$vartype/$subset.vcf.gz -p $outdir/$vartype/$subset.stats &
-
-done
-
-done
-
 wait
 
 echo "Getting the different counts"
--- marghoob/gen_tables.sh
+++ marghoob/gen_tables.sh
@@ -12,26 +12,24 @@ labels["1"]="$label1 only"
 labels["2"]="$label2 only"
 
 # Get counts for the SNP tables
-for subset in SNP INDEL
-do
+for filter in ALL PASS; do
+  for subset in SNP INDEL; do
+    for subsubset in common 1 2; do
+      basedir="$indir/$filter/$subset"
+      total=`cat $basedir/$subsubset.stats.counts | grep -v "^#" | head -n 1 | awk '{print $1}'`
+      titv=`cat $basedir/$subsubset.stats.tstv | grep -v "^#" | head -n 1 | awk '{print $3}'`
+      total_known=`cat $basedir/$subsubset/known.counts | grep -v "^#" | head -n 1 | awk '{print $1}'`
+      known_titv=`cat $basedir/$subsubset/known.tstv | grep -v "^#" | head -n 1 | awk '{print $3}'`
+      novel_titv=`cat $basedir/$subsubset/novel.tstv | grep -v "^#" | head -n 1 | awk '{print $3}'`
+      known_ratio=`echo "scale=2; 100.0*$total_known/$total"|bc -l`
+      novel_ratio=`echo "scale=2; 100.0 - $known_ratio"|bc -l`
 
-for subsubset in common 1 2
-do
-total=`cat $indir/$subset/$subsubset.stats.counts | grep -v "^#" | head -n 1 | awk '{print $1}'`
-titv=`cat $indir/$subset/$subsubset.stats.tstv | grep -v "^#" | head -n 1 | awk '{print $3}'`
-total_known=`cat $indir/$subset/$subsubset/known.counts | grep -v "^#" | head -n 1 | awk '{print $1}'`
-known_titv=`cat $indir/$subset/$subsubset/known.tstv | grep -v "^#" | head -n 1 | awk '{print $3}'`
-novel_titv=`cat $indir/$subset/$subsubset/novel.tstv | grep -v "^#" | head -n 1 | awk '{print $3}'`
-known_ratio=`echo "scale=2; 100.0*$total_known/$total"|bc -l`
-novel_ratio=`echo "scale=2; 100.0 - $known_ratio"|bc -l`
-
-echo ${labels[$subsubset]}
-echo "Sites	$subset	Ti/Tv"
-echo "All	$total	$titv"
-echo "Known	$known_ratio	$known_titv"
-echo "Novel	$novel_ratio	$novel_titv"
-echo ""
-
-done
-
+      echo "$filter	${labels[$subsubset]}"
+      echo "Sites	$subset	Ti/Tv"
+      echo "All	$total	$titv"
+      echo "Known	$known_ratio	$known_titv"
+      echo "Novel	$novel_ratio	$novel_titv"
+      echo ""
+    done
+  done
 done
