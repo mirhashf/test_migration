eb138248a37d98aa2de99c8d8097c63962328052
John Mu <johnmu@umnhoj.com>
2015-08-14 14:33:22 -0700

    fix script

--- johnmu/scripts/filter_fastq_bad_qual_length.py
+++ johnmu/scripts/filter_fastq_bad_qual_length.py
@@ -3,34 +3,30 @@
 # Filter out read pairs where there are inconsistent quality score lengths in at least one pair
 
 import sys
-import fileinput
 
 if len(sys.argv) < 2:
 	print("Usage: " + sys.argv[0] + " read1.fq read2.fq")
 	sys.exit(1)
 
-with open(sys.argv[1]) as fq1, open(sys.argv[2]) as fq2:
+with open(sys.argv[1]) as fq1, open(sys.argv[2]) as fq2, open("fixed_"+sys.argv[1],'w') as fixed_fq1, open("fixed_"+sys.argv[2],'w') as fixed_fq2:
 	line_counter = 0
+	line_buffer = list()
 	for line1, line2 in zip(fq1,fq2):
 		line1 = line1.strip()
 		line2 = line2.strip()
-
-
-for line in fileinput.input(sys.argv[1]):
-	if first_line:
-		first_line = False
-		continue
-
-	line = line.strip()
-	if len(line) == 0:
-		continue
-
-	ll = line.split()
-
-	chr = ll[0]
-	start = int(ll[1])
-	end = int(ll[2])
-	lens = [int(i) for i in ll[10].split(',')]
-
-	print '\t'.join([chr,str(start + lens[0]),str(end - lens[1])])
-
+		line_buffer.append((line1,line2))
+		line_counter += 1
+		if line_counter == 4:
+			# Check that the line lengths match
+			good_line = True
+			if len(line_buffer[1][0]) != len(line_buffer[3][0]):
+				good_line = False
+			if len(line_buffer[1][1]) != len(line_buffer[3][1]):
+				good_line = False
+			if good_line:
+				# output the files
+				for rec in line_buffer:
+					fixed_fq1.write(rec[0] + '\n')
+					fixed_fq2.write(rec[1] + '\n')
+			line_counter = 0
+			line_buffer.clear()
